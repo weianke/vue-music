8b46db1ac304069427312d4246292da83fd744db
anke <245783065@qq.com>
2017-07-10 22:54:38 +0800

    页面基本框架组建

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import fastclick from 'fastclick'
+import router from './router'
+
+import 'common/stylus/index.styl'
+
+/* eslint-disable no-unused-vars */
+// import vConsole from 'vconsole'
+
+fastclick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})

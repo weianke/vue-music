ffba518588dacadae3729a8318c857d97e2478d9
anke <245783065@qq.com>
2017-07-11 08:06:47 +0800

    歌手详情更新

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -4,7 +4,7 @@ import {commonParams, options} from './config'
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
-  const data = Object.assign({},commonParams,{
+  const data = Object.assign({}, commonParams, {
     platform: 'h5',
     uin: 0,
     needNewCode: 1

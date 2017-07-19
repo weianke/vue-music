e9d22b290c5d9e5938e88fb1b624ec8aaa16fc01
anke <245783065@qq.com>
2017-07-19 09:29:45 +0800

    推荐页面开发完毕

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,36 +1,40 @@
 <template>
   <div class="recommend">
-    <scroll class="recommend-content" :data="discList">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
       <div>
-          <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
-            <slider>
-              <div v-for="item in recommends">
-                <a :href="item.linkUrl">
-                  <img :src="item.picUrl">
-                </a>
-              </div>
-            </slider>
-          </div>
-          <div class="recommend-list">
-            <h1 class="list-title">热门歌单推荐</h1>
-            <ul>
-              <li v-for="item in discList" class="item">
-                <div class="icon">
-                  <img width="60" height="60" :src="item.imgurl" alt="">
-                </div>
-                <div class="text">
-                  <h2 class="name" v-html="item.creator.name"></h2>
-                  <p class="desc" v-html="item.dissname"></p>
-                </div>
-              </li>
-            </ul>
-          </div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <slider>
+            <div v-for="item in recommends">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
         </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Loading from 'base/loading/loading'
   import Scroll from 'base/scroll/scroll'
   import Slider from 'base/slider/slider'
   import {getRecommend, getDiscList} from 'api/recommend'
@@ -61,11 +65,18 @@
             this.discList = res.data.list
           }
         })
+      },
+      loadImage() {
+        if (!this.checkLoaded) {
+          this.$refs.scroll.refresh()
+          this.checkLoaded = true
+        }
       }
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App'
 import fastclick from 'fastclick'
 import router from './router'
+import VueLazyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
@@ -10,6 +11,10 @@ import 'common/stylus/index.styl'
 // import vConsole from 'vconsole'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({

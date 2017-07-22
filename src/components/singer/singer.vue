63e2ccfd11b35b76d67a07b1edb455de4e073aaa
anke <245783065@qq.com>
2017-07-22 16:26:04 +0800

    歌手信息展示和滚动完成

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singer">
-    歌手页面
+    <Listview :data="singers"></Listview>
   </div>
 </template>
 
@@ -8,6 +8,7 @@
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
+  import Listview from 'base/listview/listview'
 
   const HOT_NAME = '热门'
   const HOT_SINGER_LEN = 10
@@ -25,8 +26,7 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-            this.singers = res.data.list
-            console.log(this._normalizeSinger(this.singers))
+            this.singers = this._normalizeSinger(res.data.list)
           }
         })
       },
@@ -72,6 +72,9 @@
         })
         return hot.concat(ret)
       }
+    },
+    components: {
+      Listview
     }
   }
 </script>

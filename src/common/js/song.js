2590911b0df08f78fbb8c76c2574cde8f03e4298
anke <245783065@qq.com>
2018-05-27 18:41:36 +0800

    新建歌手详情组件

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,6 +1,6 @@
-import {getLyric} from 'api/song'
-import {ERR_OK} from 'api/config'
-import {Base64} from 'js-base64'
+// import {getLyric} from 'api/song'
+// import {ERR_OK} from 'api/config'
+// import {Base64} from 'js-base64'
 
 export default class Song {
   constructor({id, mid, singer, name, album, duration, image, url}) {
@@ -13,25 +13,20 @@ export default class Song {
     this.image = image
     this.url = url
   }
-
-  getLyric() {
-    if (this.lyric) {
-      return Promise.resolve(this.lyric)
-    }
-
-    return new Promise((resolve, reject) => {
-      getLyric(this.mid).then((res) => {
-        if (res.retcode === ERR_OK) {
-          this.lyric = Base64.decode(res.lyric)
-          resolve(this.lyric)
-        } else {
-          reject('no lyric')
-        }
-      })
-    })
-  }
 }
 
+// export function createSong(musicData) {
+//   return new Song({
+//     id: musicData.songid,
+//     mid: musicData.songmid,
+//     singer: filterSinger(musicData.singer),
+//     name: musicData.songname,
+//     album: musicData.albumname,
+//     duration: musicData.interval,
+//     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+//     url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+//   })
+// }
 export function createSong(musicData) {
   return new Song({
     id: musicData.songid,
@@ -55,4 +50,3 @@ function filterSinger(singer) {
   })
   return ret.join('/')
 }
-
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,55 +1,72 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :songs="songs" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
-  import {mapGetters} from 'vuex'
-  import {getSingerDetail} from 'api/singer'
-  import {ERR_OK} from 'api/config'
+import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import { createSong } from 'common/js/song'
+import MusicList from 'components/music-list/music-list'
 
-  export default {
-    computed: {
-      ...mapGetters([
-        'singer'
-      ])
-    },
-    created() {
-      console.log(this.singer)
-      this._getDetail()
-    },
-    methods: {
-      _getDetail() {
-        if (!this.singer.id) {
-          this.$router.push('/singer')
-          return
-        }
-        getSingerDetail(this.singer.id).then((res) => {
-          if (res.code === ERR_OK) {
-            console.log(res.data.list)
-          }
-        })
-      }
+export default {
+  data () {
+    return {
+      songs: []
     }
+  },
+  computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
+    ...mapGetters([
+      'singer'
+    ])
+  },
+  created () {
+    console.log(this.singer)
+    this._getDetail()
+  },
+  methods: {
+    _getDetail () {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+        return
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs (list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albumid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  },
+  components: {
+    MusicList
   }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable'
 
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
-
-  .slide-enter-active, .slide-leave-active
-    transition: all 0.3s
-
-  .slide-enter, .slide-leave-to
-    transform: translate3D(100%, 0, 0)
+.slide-enter-active, .slide-leave-active
+  transition: all 0.3s
+.slide-enter, .slide-leave-to
+  transform: translate3D(100%, 0, 0)
 </style>

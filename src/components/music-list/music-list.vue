9b740cb9714e8e8e6d4b31321344d52f11fa77be
anke <245783065@qq.com>
2018-05-28 15:37:29 +0800

    更新song-list组件

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -4,13 +4,21 @@
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
-    <div class="bg-image" :style="bgStyle">
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <scroll :data="songs" class="list" ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script  type='text/ecmascript-6'>
+import Scroll from 'base/scroll/scroll'
+import SongList from 'base/song-list/song-list'
+
 export default {
   props: {
     bgImage: {
@@ -30,6 +38,13 @@ export default {
     bgStyle () {
       return `background-image:url(${this.bgImage})`
     }
+  },
+  mounted() {
+    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+  },
+  components: {
+    Scroll,
+    SongList
   }
 }
 </script>
